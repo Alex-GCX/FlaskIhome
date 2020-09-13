df793188e1c782db0e07a19add4b57c3845706a0
Alex-GCX <g1242556827@163.com>
2020-09-13 21:37:28 +0800

    1.0

--- manager.py
+++ manager.py
@@ -10,7 +10,7 @@ if __name__ != '__main__':
     # 使用gunicorn启动时, 将flask应用中的日志绑定到gunicorn的日志配置中
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel('INFO')
+    app.logger.setLevel(gunicorn_logger.level)
 
 # 创建迁移对象
 migrate = Migrate(app, db)
